1ad0c8cc33c2566986789d53817926fe0826bd8d
GargantuanGarfield <145381852+GargantuanGarfield@users.noreply.github.com>
2025-01-24 10:02:16 -0500

    Add files via upload

---
+++ Stairs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+class Stairs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("thing :3:3:3 -- ");
+        String character = input.nextLine();
+        System.out.println("Number :3:3:3 -- ");
+        int number = input.nextInt();
+        for (int i = number; i >= 0; --i) {
+            String output = "";
+            output += " ".repeat(i);
+            output += character;
+            for (int l = output.length(); l <= number; l++){
+                output += character;
+            }
+            System.out.println(output);
+        }
+        
+    }
+}
