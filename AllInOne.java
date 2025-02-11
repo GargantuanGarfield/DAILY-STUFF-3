5de5fa3d23010b563ea101bed8270ac8960054c3
GargantuanGarfield <145381852+GargantuanGarfield@users.noreply.github.com>
2025-02-11 08:20:07 -0500

    :3

--- AllInOne.java
+++ AllInOne.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class AllInOne {
     public static int sum(int num1, int num2, int num3){
         int sum = num1 + num2 + num3;
         return sum;
